a759302b4a83fb494226352c88f5e60dd451807b
isso-719 <i@kazukiisogai.net>
2023-02-04 17:11:51 +0900

    add dotnet

--- HomeBrewSoftwares/install_Visual_Studio.sh
+++ HomeBrewSoftwares/install_Visual_Studio.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+# Install Homebrew Cask Versions
+brew tap homebrew/cask-versions
+
 # Install Visual Studio
 brew install --cask visual-studio
---
+++ HomeBrewSoftwares/install_dotnet_sdk.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+# Install .NET Core SDK
+brew install --cask dotnet-sdk
