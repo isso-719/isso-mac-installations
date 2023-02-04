2a703a81ea2883920908fdd90ab93ed542dc9129
isso-719 <i@kazukiisogai.net>
2023-02-04 13:45:36 +0900

    fix HomeBrew

--- HomeBrew/install_HomeBrew.sh
+++ HomeBrew/install_HomeBrew.sh
@@ -2,3 +2,6 @@
 
 # Install HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add HomeBrew to PATH
+echo '\n# HomeBrew\nexport PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
